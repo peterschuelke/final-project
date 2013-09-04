311004efe468eaf3184b38ea952969fdecc99844
Peter <peter.c.schueke@gmail.com>
2013-09-03 21:45:47 -0700

    step-24

--- app/js/controllers/RestaurantsController.js
+++ app/js/controllers/RestaurantsController.js
@@ -13,10 +13,10 @@ foodMeApp.controller('RestaurantsController',
      rating: null
    };
 
-  var allRestaurants = Restaurant.query(filterRestaurants);
-  $scope.$watch('filter', filterRestaurants, true);
+  var allRestaurants = Restaurant.query(filterAndSortRestaurants);
+  $scope.$watch('filter', filterAndSortRestaurants, true);
 
-  function filterRestaurants() {
+  function filterAndSortRestaurants() {
     $scope.restaurants = [];
 
     // filter
@@ -31,6 +31,53 @@ foodMeApp.controller('RestaurantsController',
 
       $scope.restaurants.push(item);
     });
+
+      // sort
+    $scope.restaurants.sort(function(a, b) {
+      if (a[filter.sortBy] > b[filter.sortBy]) {
+        return filter.sortAsc ? 1 : -1;
+      }
+
+      if (a[filter.sortBy] < b[filter.sortBy]) {
+        return filter.sortAsc ? -1 : 1;
+      }
+
+      return 0;
+    });
+  };
+
+  $scope.sortBy = function(key) {
+    if (filter.sortBy === key) {
+      filter.sortAsc = !filter.sortAsc;
+    } else {
+      filter.sortBy = key;
+      filter.sortAsc = true;
+    }
+   };
+
+
+   $scope.sortIconFor = function(key) {
+    if (filter.sortBy !== key) {
+      return '';
+    }
+
+    return filter.sortAsc ? '\u25B2' : '\u25BC';
+   };
+
+  $scope.CUISINE_OPTIONS = {
+    african: 'African',
+    american: 'American',
+    barbecue: 'Barbecue',
+    cafe: 'Cafe',
+    chinese: 'Chinese',
+    'czech/slovak': 'Czech / Slovak',
+    german: 'German',
+    indian: 'Indian',
+    japanese: 'Japanese',
+    mexican: 'Mexican',
+    pizza: 'Pizza',
+    thai: 'Thai',
+    vegetarian: 'Vegetarian'
   };
 
 });
